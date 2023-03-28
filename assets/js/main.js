680208ff9917f2099a002c23be97379c3503bfac
Marcelogmuniz <marcelogmuniz@outlook.com>
2023-03-28 15:40:48 -0300

    Projeto quase finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('photo')
-    photo.src = profileData.photo 
-    photo.alt = profileData.name
+    // const photo = document.getElementById('photo')
+    // photo.src = profileData.photo 
+    // photo.alt = profileData.name
 
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
@@ -14,14 +14,22 @@ function updateProfileInfo(profileData) {
 
     const phone = document.getElementById('profile.phone')
     phone.innerText = profileData.phone
-    phone.href = 'tel:${profileData.phone}'
+    phone.href = `tel:${profileData.phone}`
 
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
     
 }
 
-(async function ( ) {
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+
+    softSkills.innerhtml = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+(async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSkills(profileData)
 })()
